59737234a5b7ca612b8b56b856f93c2a8dbfc7a4
Falase Femi <femifalase228@gmail.com>
2023-03-04 17:07:43 +0100

    done witht he expense app

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
 
 const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
     <div>
-        <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h4>Your Balance</h4>
+      <h1>${total}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Balance
+export default Balance;
