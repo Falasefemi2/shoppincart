6d54306e96eecd3d7d344198eadbf29adf9e1acc
Falase Femi <femifalase228@gmail.com>
2023-03-04 17:04:26 +0100

    added all components to aoo.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import Header from './components/Header'
-import Balance from './components/Balance'
-import IncomeExpenses from './components/IncomeExpenses'
-import TransactionList from './components/TransactionList'
-import AddTransactionList from './components/AddTransactionList'
-
-
+import React from "react";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransactionList from "./components/AddTransactionList";
+import { GlobalProvider } from "./context/GlobalState";
 
 const App = () => {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
         <IncomeExpenses />
-    <TransactionList />
-    <AddTransactionList />
+        <TransactionList />
+        <AddTransactionList />
       </div>
-    </div>
-  )
-}
+    </GlobalProvider>
+  );
+};
 
-export default App
+export default App;
